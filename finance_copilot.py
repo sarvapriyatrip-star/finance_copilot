5d77c2cdf2df027798e3ac800f2ffb2f4d0fdbdd
sarvapriyatrip-star <sarvapriyatrip@gmail.com>
2026-06-25 18:42:18 +0530

    Second Commit

--- finance_copilot.py
+++ finance_copilot.py
@@ -9,7 +9,7 @@ api_key = os.getenv("GOOGLE_API_KEY")
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key=api_key,
+    GOOGLE_API_KEY=api_key,
     temperature=0.2
 )
 
